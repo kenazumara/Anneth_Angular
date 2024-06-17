fa6640a0b7df47216295ac78d583e6e05cd96d91
Kenneth Azumara <kenazumara@gmail.com>
2024-06-17 12:20:44 +0100

    User profile information

--- src/app/user/profile-info/profile-info.component.ts
+++ src/app/user/profile-info/profile-info.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from '../user';
-import { AuthService } from 'src/app/shared/auth.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-profile-info',
