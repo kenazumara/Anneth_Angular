2bfe58c67a7f7803398f90b20724cb6a6332a6b3
Kenneth Azumara <kenazumara@gmail.com>
2024-06-17 14:46:03 +0100

    Updated product module

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,42 +1,3 @@
-// // src/app/product/product.module.ts
-// import { NgModule } from '@angular/core';
-// import { RouterModule, Routes } from '@angular/router';
-// import { StoreModule } from '@ngrx/store';
-// import { EffectsModule } from '@ngrx/effects';
-
-// import { SharedModule } from '../shared/shared.module';
-// import { ProductListComponent } from './product-list/product-list.component';
-// import { ProductDetailsComponent } from './product-details/product-details.component';
-// import { CarouselComponent } from '../carousel/carousel.component';
-// import { SimilarProductsComponent } from './similar-products/similar-products.component';
-// import { ProductOptionComponent } from './product-option/product-option.component';
-// import { productReducer } from './state/product.reducer';
-// import { ProductEffects } from './state/product.effects';
-
-// const productRoutes: Routes = [
-//   { path: 'products', component: ProductListComponent },
-//   { path: 'products/:id', component: ProductDetailsComponent },
-//   { path: '', redirectTo: 'products', pathMatch: 'full' },
-// ];
-
-// @NgModule({
-//   declarations: [
-//     ProductListComponent,
-//     ProductDetailsComponent,
-//     CarouselComponent,
-//     SimilarProductsComponent,
-//     ProductOptionComponent
-//   ],
-//   imports: [
-//     SharedModule, // Import SharedModule to include CommonModule and FormsModule
-//     RouterModule.forChild(productRoutes),
-//     EffectsModule.forFeature([ProductEffects]),
-//     StoreModule.forFeature('products', productReducer),
-//   ],
-// })
-// export class ProductModule {}
-
-
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
@@ -45,14 +6,16 @@ import { EffectsModule } from '@ngrx/effects';
 import { SharedModule } from '../shared/shared.module';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { CarouselComponent } from '../carousel/carousel.component';
 import { SimilarProductsComponent } from './similar-products/similar-products.component';
 import { ProductOptionComponent } from './product-option/product-option.component';
 import { productReducer } from './state/product.reducer';
 import { ProductEffects } from './state/product.effects';
+import { ProductReviewsComponent } from '../user/user-reviews/user-reviews.component';
+import { CarouselComponent } from '../home/carousel/carousel.component';
 
 const productRoutes: Routes = [
   { path: 'products', component: ProductListComponent },
+  { path: 'products/review', component: ProductReviewsComponent },
   { path: 'products/:id', component: ProductDetailsComponent },
   { path: '', redirectTo: 'products', pathMatch: 'full' },
 ];
@@ -63,7 +26,7 @@ const productRoutes: Routes = [
     ProductDetailsComponent,
     CarouselComponent,
     SimilarProductsComponent,
-    ProductOptionComponent
+    ProductOptionComponent,
   ],
   imports: [
     SharedModule,
