227a761bdfead459b270842add634426cb6646b3
Kenneth Azumara <kenazumara@gmail.com>
2024-06-17 13:04:45 +0100

    Better code

--- src/app/product/similar-products/similar-products.component.ts
+++ src/app/product/similar-products/similar-products.component.ts
@@ -1,66 +1,131 @@
-import { Component, Input, OnInit } from '@angular/core';
+// import { Component, Input, OnInit } from '@angular/core';
+// import { ProductService } from '../product.service';
+// import { Product } from '../product';
+// import {
+//   ActivatedRoute,
+//   ActivatedRouteSnapshot,
+//   NavigationEnd,
+//   Router,
+// } from '@angular/router';
+
+// @Component({
+//   selector: 'similar-products',
+//   templateUrl: './similar-products.component.html',
+//   styleUrls: ['./similar-products.component.css'],
+// })
+// export class SimilarProductsComponent implements OnInit {
+//   pageTitle: string = 'Products You May Love';
+//   @Input() products!: Product[] | null;
+//   product!: Product;
+
+//   constructor(
+//     private productService: ProductService,
+//     private route: ActivatedRoute,
+//     private router: Router
+//   ) {
+//     this.router.events.subscribe((event) => {
+//       if (event instanceof NavigationEnd) {
+//         this.scrollToTop();
+//       }
+//     });
+//   }
+
+//   ngOnInit(): void {
+//     const param = this.route.snapshot.paramMap.get('id');
+//     const id = param;
+//     this.getProducts(id);
+//   }
+
+//   getProducts(id: any) {
+//     this.productService.getProduct(id).subscribe((product) => {
+//       this.product = product;
+//       this.router.navigate(['/products', product.id]);
+//     });
+//   }
+
+//   isProductIdValid(id: any): boolean {
+//     return !isNaN(id) && typeof id === 'number';
+//   }
+
+//   onBack(productId: string):any {
+//     setTimeout(() => {
+//       // Navigate to the desired route
+//       this.router.navigate(['/products', productId]);
+
+//       // Hide the spinner (toggle the variable for spinner visibility)
+//     }, 1000);
+//     // location.reload();
+//   }
+
+//   scrollToTop() {
+//     // Scroll to the top of the page
+//     document.body.scrollTop = 0; // For Safari
+//     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+//   }
+
+// }
+
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from '../product';
-import {
-  ActivatedRoute,
-  ActivatedRouteSnapshot,
-  NavigationEnd,
-  Router,
-} from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'similar-products',
   templateUrl: './similar-products.component.html',
   styleUrls: ['./similar-products.component.css'],
 })
-export class SimilarProductsComponent implements OnInit {
-  pageTitle: string = 'Products You May Love';
+export class SimilarProductsComponent implements OnInit, OnDestroy {
+  pageTitle = 'Products You May Love';
   @Input() products!: Product[] | null;
-  product!: Product;
+  product: Product | null = null;
+  errorMessage!: string;
+  private routerEventsSubscription: Subscription | null = null;
 
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute,
     private router: Router
-  ) {
-    this.router.events.subscribe((event) => {
+  ) {}
+
+  ngOnInit(): void {
+    this.routerEventsSubscription = this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         this.scrollToTop();
       }
     });
-  }
 
-  ngOnInit(): void {
     const param = this.route.snapshot.paramMap.get('id');
-    const id = param;
-    this.getProducts(id);
+    if (param) {
+      this.getProducts(param);
+    }
   }
 
-  getProducts(id: any) {
-    this.productService.getProduct(id).subscribe((product) => {
-      this.product = product;
-      this.router.navigate(['/products', product.id]);
+  ngOnDestroy(): void {
+    if (this.routerEventsSubscription) {
+      this.routerEventsSubscription.unsubscribe();
+    }
+  }
+
+  getProducts(id: string): void {
+    this.productService.getProduct(id).subscribe({
+      next: (product) => {
+        this.product = product;
+        this.router.navigate(['/products', product.id]);
+      },
+      error: (error) => {
+        console.error('Error fetching product:', error);
+        this.errorMessage = error;
+      },
     });
   }
 
-  isProductIdValid(id: any): boolean {
-    return !isNaN(id) && typeof id === 'number';
+  onBack(productId: string): void {
+    this.router.navigate(['/products', productId]);
   }
 
-  onBack(productId: string):any {
-    setTimeout(() => {
-      // Navigate to the desired route
-      this.router.navigate(['/products', productId]);
-
-      // Hide the spinner (toggle the variable for spinner visibility)
-    }, 1000);
-    // location.reload();
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
-
-  scrollToTop() {
-    // Scroll to the top of the page
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
-  }
-
 }
