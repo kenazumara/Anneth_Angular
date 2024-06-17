5b408904be8a751ef7fd59cde720f2d56d52c7ac
Kenneth Azumara <kenazumara@gmail.com>
2024-06-17 14:59:25 +0100

    Rxjs, added error handler

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -1,15 +1,12 @@
 import {
   HttpClient,
   HttpErrorResponse,
-  HttpHeaders,
-  HttpRequest,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {
   Observable,
-  Subject,
+  BehaviorSubject,
   catchError,
-  combineLatest,
   map,
   shareReplay,
   tap,
@@ -22,63 +19,57 @@ import { Product } from './product';
   providedIn: 'root',
 })
 export class ProductService {
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+  isLoading$ = this.loadingSubject.asObservable();
+
   constructor(private http: HttpClient) {}
-  isLoading: boolean = true;
 
-  getProducts() {
-    const url = 'http://127.0.0.1:5000/api/v1';
-    return this.http.get<any>(url + '/products').pipe(
+  getProducts(): Observable<Product[]> {
+    const url = 'http://127.0.0.1:5000/api/v1/products';
+    this.loadingSubject.next(true);
+    return this.http.get<any>(url).pipe(
       map((data) => data.data.data),
+      tap(() => this.loadingSubject.next(false)),
       shareReplay(1),
-      tap((data) => {
-        console.log(data);
-        if (data) this.isLoading = false;
-      }),
-      shareReplay(1),
-      catchError(this.handleError)
+      catchError((error) => {
+        this.loadingSubject.next(false);
+        return this.handleError(error);
+      })
     );
-  }
-
-  getprod() {
-    const request = new HttpRequest(
-      'GET',
-      'http://127.0.0.1:5000/api/v1/products',
-      { reportProgress: true }
-    );
-    return this.http.request(request);
   }
 
   getProduct(id: string): Observable<Product> {
-    const url = 'http://127.0.0.1:5000/api/v1/products';
-    return this.http.get<any>(`${url}/${id}`).pipe(
+    const url = `http://127.0.0.1:5000/api/v1/products/${id}`;
+    this.loadingSubject.next(true);
+    return this.http.get<any>(url).pipe(
       map((data) => data.data.data),
-      tap((data) => {
-        console.log(data);
-        if (data) this.isLoading = false;
-      }),
-      catchError(this.handleError)
+      tap(() => this.loadingSubject.next(false)),
+      catchError((error) => {
+        this.loadingSubject.next(false);
+        return this.handleError(error);
+      })
     );
   }
 
-  getSimilarProducts(id: string) {
+  getSimilarProducts(id: string): Observable<Product[]> {
     const url = `http://127.0.0.1:5000/api/v1/products/${id}/relatedProducts`;
+    this.loadingSubject.next(true);
     return this.http.get<any>(url).pipe(
       map((data) => data.data.products),
-      catchError(this.handleError)
+      tap(() => this.loadingSubject.next(false)),
+      catchError((error) => {
+        this.loadingSubject.next(false);
+        return this.handleError(error);
+      })
     );
   }
 
   private handleError(err: HttpErrorResponse): Observable<never> {
-    // in a real world app, we may send the server to some remote logging infrastructure
-    // instead of just logging it to the console
     let errorMessage = '';
     if (err.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
       errorMessage = `An error occurred: ${err.error.message}`;
     } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      errorMessage = `${err.error.status}: ${err.error.message}`;
+      errorMessage = `${err.status}: ${err.message}`;
     }
     console.error(errorMessage);
     return throwError(() => errorMessage);
