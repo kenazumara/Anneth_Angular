5b7bc92900896f14ab6eeb57125a276dc3bc5f42
Kenneth Azumara <kenazumara@gmail.com>
2024-06-18 13:09:56 +0100

    Removed deleted files

--- src/app/state/action/app-page-action.ts
+++
@@ -1,3 +0,0 @@
-import { createAction } from "@ngrx/store";
-
-export const toggleNavBar = createAction('[App Page] Toggle navigation bar] ')
--- src/app/state/index.ts
+++
@@ -1,4 +0,0 @@
-import { createFeatureSelector } from "@ngrx/store";
-import { InitialAppState } from "./reducer";
-
-export const getAppFeature = createFeatureSelector<InitialAppState>('')
