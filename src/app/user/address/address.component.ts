f52e1a8c40dd99046a22df37e368c6fccadb7b8a
Kenneth Azumara <kenazumara@gmail.com>
2024-06-18 13:22:17 +0100

    Retrived user details to display for better UI experience

--- src/app/user/address/address.component.ts
+++ src/app/user/address/address.component.ts
@@ -1,22 +1,78 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/shared/auth.service';
-import { User } from '../user';
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
+import { Address, User } from '../user';
+import { AuthService } from '../auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-address',
   templateUrl: './address.component.html',
-  styleUrls: ['./address.component.css']
+  styleUrls: ['./address.component.css'],
 })
 export class AddressComponent implements OnInit {
-  user!: User
+  user!: User;
+  addressForm!: FormGroup;
+  showForm = false;
+  isMenuOpen!: boolean;
 
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder,
+  ) {}
 
-  constructor(private authService: AuthService) { }
-
-  ngOnInit(): void {
-    this.authService.getUser().subscribe((user)=>
-    this.user = user
-    )
+  get f() {
+    return this.addressForm.controls;
   }
 
+  ngOnInit(): void {
+    this.authService.getUser().subscribe((user) => {
+      this.user = user;
+      this.addressForm.patchValue({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        number: user.phone,
+      });
+    });
+
+    this.addressForm = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      number: [''],
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      country: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.addressForm.valid) {
+      const address: Address = this.addressForm.value;
+      this.authService.createAddress(address).subscribe((val) => {
+        console.log(val);
+        this.authService.getUser().subscribe((user) => {
+          this.user = user;
+          this.addressForm.reset({
+            street: '',
+            city: '',
+            state: '',
+            country: '',
+          })
+          this.showForm = false;
+
+        });
+      });
+    }
+  }
+
+  showAddForm() {
+    this.showForm = true;
+  }
+
+  back() {
+    this.showForm = false;
+this.isMenuOpen = false
+  }
 }
